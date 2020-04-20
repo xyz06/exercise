aa9c511ffb7d948646349b3151e3f4513932dc8d
xyz06 <1056156342@qq.com>
2020-04-20 13:58:56 +0800

    add unit-4

---
+++ unit-4/print_with_width.py
@@ -0,0 +1,32 @@
+import re
+import argparse
+
+
+def print_with_width(text, width):
+    n = width
+    print(text[0:n])
+    while n < len(text):
+
+        if re.search(r"[a-zA-Z]", text[n]):
+            if text[n - 1] == " ":
+                print(text[n:n + width])
+                n = n + width
+            else:
+                print("-", end="")
+                print(text[n:n + width - 1])
+                n = n + width - 1
+        else:
+            if text[n] == " ":
+                print(text[n + 1:n + 1 + width])
+                n = n + 1 + width
+            else:
+                print(text[n:n + width])
+                n = n + width
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--text", type=str, required=True, help="input some English words")
+    parser.add_argument("--width", type=int, required=True, help="it is per line width")
+    args = parser.parse_args()
+    print_with_width(args.text, args.width)
