f9964efa5d60213c8151c28c4029ac36facf5817
xyz06 <1056156342@qq.com>
2020-04-22 15:54:27 +0800

    modify until-4

--- unit-4/print_with_width.py
+++ unit-4/print_with_width.py
@@ -2,25 +2,49 @@ import re
 import argparse
 
 
-def print_with_width(text, width=80):
-    n = width
-    print(text[0:n])
-    while n < len(text):
+def fristword_upper(text):
+    words = text[0].upper()
+    flag = False
+    for i in range(1, len(text)):
+        if flag:
+            if text[i] == " ":
+                flag = True
+                words += text[i]
+                continue
+            else:
+                flag = False
+                words += text[i].upper()
+                continue
+        words += text[i]
+        if text[i] == "." and i < len(text) - 1:
+            flag = True
+            continue
+    return words
 
-        if re.search(r"[a-zA-Z]", text[n]):
-            if text[n - 1] == " ":
-                print(text[n:n + width])
+
+def print_with_width(text, width, paragraph):
+    n = width
+    if paragraph == True:
+        words = fristword_upper(text)
+    else:
+        words = text
+
+    print(words[0:n])
+    while n < len(words):
+        if re.search(r"[a-zA-Z]", words[n]):
+            if words[n - 1] == " ":
+                print(words[n:n + width])
                 n = n + width
             else:
                 print("-", end="")
-                print(text[n:n + width - 1])
+                print(words[n:n + width - 1])
                 n = n + width - 1
         else:
-            if text[n] == " ":
-                print(text[n + 1:n + 1 + width])
+            if words[n] == " ":
+                print(words[n + 1:n + 1 + width])
                 n = n + 1 + width
             else:
-                print(text[n:n + width])
+                print(words[n:n + width])
                 n = n + width
 
 
@@ -28,5 +52,9 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--text", type=str, required=True, help="input some English words")
     parser.add_argument("--width", type=int, default=80, help="it is per line width")
+    parser.add_argument("--paragraph", default="False", action="store_true")
     args = parser.parse_args()
-    print_with_width(args.text, args.width)
+    print_with_width(args.text, args.width, args.paragraph)
+
+
+
