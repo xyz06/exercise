e6b8d5a7d8d4bf990d2ac2ea8e57c43f5ee98b32
xyz06 <1056156342@qq.com>
2020-04-27 21:15:30 +0800

    modify unit-5

--- unit-5/running_title.py
+++ unit-5/running_title.py
@@ -25,52 +25,53 @@ def runing_text(title,color):
     myfont = pygame.font.Font(None, 60)
     mytitle = myfont.render(title, True, colors[color])
     ztx, zty, ztw, zth = mytitle.get_rect()
+    try:
+        flag = True
+        while 1:
+            for event in pygame.event.get():
+                if event.type in (QUIT, KEYDOWN):
+                    sys.exit()
 
-    flag = True
-    while 1:
-        for event in pygame.event.get():
-            if event.type in (QUIT, KEYDOWN):
-                sys.exit()
+            screen.fill((0, 0, 0))
 
-        screen.fill((0, 0, 0))
-
-        x += vel_x
-        if x >= 610 and flag:
-
-            y = y + 20
-            x = 0
-
-        if y > 260 and x == 0 and flag:
-            print("Start the collision")
-            flag = False
-            y = 260
-            vel_x = -vel_x
-            vel_y = -vel_y
-
-        #after collision change color
-        if not flag:  # x += vel_x
-            y += vel_y
-            if x > 600 - ztw or x < 0:
-                vel_x = -vel_x
-                mytitle = myfont.render(title, True, random.choice(list(colors.values())))
-            if y > 260 or y < 0:
-                mytitle = myfont.render(title, True, random.choice(list(colors.values())))
-                vel_y = -vel_y
-
-            if y == 260 and x == 102:
-                flag = True
-                vel_y = -vel_y
+            x += vel_x
+            if x >= 610 and flag:
+                y = y + 20
                 x = 0
-                y = 0
 
-        screen.blit(mytitle, (x, y))
-        pygame.display.update()
-        time.sleep(0.006)
+            if y > 260 and x == 0 and flag:
+                print("Start the collision")
+                flag = False
+                y = 260
+                vel_x = -vel_x
+                vel_y = -vel_y
+
+            # after collision change color
+            if not flag:  # x += vel_x
+                y += vel_y
+                if x > 600 - ztw or x < 0:
+                    vel_x = -vel_x
+                    mytitle = myfont.render(title, True, random.choice(list(colors.values())))
+                if y > 260 or y < 0:
+                    mytitle = myfont.render(title, True, random.choice(list(colors.values())))
+                    vel_y = -vel_y
+
+                if y == 260 and x == 102:
+                    flag = True
+                    vel_y = -vel_y
+                    x = 0
+                    y = 0
+
+            screen.blit(mytitle, (x, y))
+            pygame.display.update()
+            time.sleep(0.006)
+    except KeyboardInterrupt:
+        sys.exit()
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--title", type=str, required=True)
-    parser.add_argument("--color", type=str, default='red', help="chocie one color [white, blue, red, orange, yellow, green] ")
+    parser.add_argument("--color", type=str, default='red', choices=['white', 'blue', 'red', 'orange', 'yellow', 'green'],help="chocie one color [white, blue, red, orange, yellow, green] ")
     args = parser.parse_args()
     runing_text(args.title,args.color)
